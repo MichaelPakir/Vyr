9ae6c6506a24212269eee8d7756a83bf308af611
MichaelPakir <neovimm@gmail.com>
2026-05-06 13:23:33 +0800

    installed dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/index.css"
+
+function App() {
+  return (
+    <section>
+      <div>
+        <h1 className="text-1xl">Hello World</h1>
+      </div>
+    </section>
+  )
+}
+
+export default App
