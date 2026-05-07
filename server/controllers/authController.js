05bcc45894578d4139e6d2357849ea954b7331df
MichaelPakir <neovimm@gmail.com>
2026-05-07 19:28:27 +0800

    backening the backend

---
+++ server/controllers/authController.js
@@ -0,0 +1,91 @@
+import User from "../models/user.js"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Please fill all fields" })
+  }
+
+  try {
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists",
+      })
+    }
+
+    const salt = await bcrypt.genSalt(12)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    })
+
+    res.status(201).json({
+      message: "user registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      token,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    })
+  }
+}
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (!userExists) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, userExists.password)
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      })
+    }
+
+    const token = jwt.sign({ id: userExists._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    })
+
+    res.status(200).json({
+      message: "Login Successful",
+      user: {
+        id: userExists._id,
+        name: userExists.name,
+        email: userExists.email,
+        role: userExists.role,
+      },
+      token,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    })
+  }
+}
+
+export default { registerUser, loginUser }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv"
+import authRoutes from "./routes/authRoutes.js"
 dotenv.config()
 
 import express from "express"
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/auth", authRoutes)
 
 app.get("/", (req, res) => {
   res.send("API is running")
