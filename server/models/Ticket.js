73eee634638e128bdad1f7e0159a630b2bb19b8e
MichaelPakir <neovimm@gmail.com>
2026-05-07 20:04:43 +0800

    changes

---
+++ server/models/Ticket.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Open", "Pending", "Resolved"],
+      default: "Open",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
+
+const Ticket = mongoose.model("Ticket", ticketSchema)
+
+export default Ticket
