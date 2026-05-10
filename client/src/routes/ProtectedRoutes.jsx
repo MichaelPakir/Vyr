8046ca74018baca2877d2ff19136ea001cc1e9e1
MichaelPakir <neovimm@gmail.com>
2026-05-10 18:23:06 +0800

    prevents unauthed users to access dashboard page

--- client/src/routes/ProtectedRoutes.jsx
+++ client/src/routes/ProtectedRoutes.jsx
@@ -1,11 +1,14 @@
-import React from "react"
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
 
 const ProtectedRoutes = () => {
-  return (
-    <div>
-      <h1>These are protected routes</h1>
-    </div>
-  )
+  const { user } = useAuth()
+
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
+
+  return <Outlet />
 }
 
 export default ProtectedRoutes
