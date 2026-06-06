4655b8aed6eb4145024d1b67de489aedc340b25d
MichaelPakir <neovimm@gmail.com>
2026-06-06 13:15:01 +0800

    fix: replace localhost with production API endpoint

--- client/src/services/usersService.js
+++ client/src/services/usersService.js
@@ -1,6 +1,7 @@
 import axios from "axios"
+import { API_BASE_URL } from "./apiConfig"
 
-const API_URL = "http://localhost:5000/api/users"
+const API_URL = `${API_BASE_URL}/api/users`
 
 export const getUsersRequest = async (token) => {
   const response = await axios.get(API_URL, {
@@ -13,29 +14,21 @@ export const getUsersRequest = async (token) => {
 }
 
 export const promoteUserRequest = async (userId, token) => {
-  const response = await axios.patch(
-    `${API_URL}/${userId}/promote`,
-    null,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+  const response = await axios.patch(`${API_URL}/${userId}/promote`, null, {
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
-  )
+  })
 
   return response.data
 }
 
 export const demoteUserRequest = async (userId, token) => {
-  const response = await axios.patch(
-    `${API_URL}/${userId}/demote`,
-    null,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+  const response = await axios.patch(`${API_URL}/${userId}/demote`, null, {
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
-  )
+  })
 
   return response.data
 }
